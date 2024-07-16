efe22fd212733719203672beccbdf96c247904fb
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-16 04:00:48 -0700

    Code Successfully

---
+++ Function.js
@@ -0,0 +1,80 @@
+
+// Function : 
+
+function sayMyName () {
+  console.log("A")
+  console.log("L")
+  console.log("I")
+  console.log("Y")
+  console.log("A")
+  console.log("H")
+  console.log("Y")
+  console.log("A")
+}
+
+sayMyName()
+
+
+// function addTwoNumbers (number1 , number2) {
+//   console.log(number1 + number2) ; 
+// }
+
+// addTwoNumbers(15, 25)
+
+ function addTwoNumbers (number1 , number2) {
+    
+  // let Result = number1 + number2 ;
+  // return Result ;
+
+  return number1 + number2 ;
+
+  }
+
+  const Result = addTwoNumbers(45, 15)
+console.log('Result : ' + Result)
+
+
+
+
+function loginUserMessage (UserNmae) {
+  if (UserNmae===undefined) {
+    return 
+  }
+  return `${UserNmae} just logged in`
+}
+
+console.log(loginUserMessage("undefined"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
